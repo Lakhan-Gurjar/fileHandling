507455ec544fad0b42769818aa645f189ad036b0
Lakhan-Gurjar <140910667+Lakhan-Gurjar@users.noreply.github.com>
2024-05-30 08:19:16 +0530

    File creation

---
+++ createFile.java
@@ -0,0 +1,19 @@
+package fileHandling;
+import java.io.*;
+
+public class createFile {
+    public static void main (String args []) {
+        /* Create a File */
+
+        File f = new File("D:\\Future\\JavaTutorialbyApnaCollege\\fileHandling\\newfile.txt");
+        try {
+            if (f.createNewFile()) {
+                System.out.println("File Successfully created.");
+            } else {
+                System.out.println("File already Exists...!");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occured : " + e.getMessage());
+        }
+    }
+}
